20840f777aee4bb73552d18977dbc8ff0ca01bf2
Souvik Daw <souvikdaw2000@gmail.com>
2026-03-01 11:55:15 +0530

    CICD pipeline test

--- microservice-1/src/main/java/com/example/microservice1/Microservice1Application.java
+++ microservice-1/src/main/java/com/example/microservice1/Microservice1Application.java
@@ -19,7 +19,7 @@ public class Microservice1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Microservice1Application.class, args);
-        System.out.println("Microservice 1 started");
+        System.out.println("Microservice 1 started ....");
     }
 
     @Bean
